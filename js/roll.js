c6b3766985e9b324c04e919ff1cde0f39550b23f
Emmanuel Flores Elias <efeapple@gmail.com>
2013-05-22 11:28:55 +0200

    added the roll link to monotone

---
+++ js/roll.js
@@ -0,0 +1,24 @@
+/* link-roll
+   */
+
+var supports3DTransforms = document.body.style['webkitPerspective'] !== undefined ||
+    document.body.style['MozPerspective'] !== undefined;
+
+function roll(selector) {
+    if (supports3DTransforms) {
+        
+        var nodes = document.querySelectorAll(selector);
+
+        for (var i=0, len=nodes.length; i<len; i++) {
+            var node = nodes[i];
+
+            if (!node.className || !node.className.match( /roll/g )) {
+                node.className += ' roll';
+                node.innerHTML = '<span data-title="'+ node.text +'">' + node.innerHTML + '</span>';
+            }
+        };
+    }
+}
+
+//Call to the function
+roll('#link-roll');
